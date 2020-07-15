780385a21b5e87705717076c4bb49ca69a04b03d
mus9004 <mus9004@yahoo.es>
2020-07-15 13:58:37 -0600

    [ADD] Relations between models

---
+++ openacademy/models/models.py
@@ -0,0 +1,10 @@
+    description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
