4c31c1b5ad40408e9fade2e893f60856efcc49e3
mus9004 <mus9004@yahoo.es>
2020-07-15 15:35:24 -0600

    [FIX]

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -17,3 +17,4 @@ class Session(models.Model):
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
