5ebb7ac86b0489dcdd0ed8808a0f3e083fcc02df
mus9004 <mus9004@yahoo.es>
2020-07-24 03:56:36 -0600

    [fix]

--- openacademy/wizard.py
+++ openacademy/wizard.py
@@ -5,9 +5,7 @@ from odoo import models, fields, api
 class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
-    
-    def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
-    
-    session_id = fields.Many2one('openacademy.session', string="Session", required=True, default=_default_session)
+
+    session_id = fields.Many2one('openacademy.session',
+        string="Session", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
