141429994de392096495fdb03c20d4033645be76
mus9004 <mus9004@yahoo.es>
2020-07-24 05:02:03 -0600

    [add] Define a Dashboard

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+        'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
